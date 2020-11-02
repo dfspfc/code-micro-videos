999a08a61018b030791b1997d85bdfbaff300b42
dfspfc <da.ferreiraalves@gmail.com>
2020-11-02 11:13:58 -0300

    Creating category and gender resources

---
+++ database/seeds/GenderSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class GenderSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * 
+     * 
+     * @return void
+     */
+    public function run()
+    {
+        factory(\App\Models\Gender::class, 5)->create(); 
+    }
+}
